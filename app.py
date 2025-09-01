2c9c355452ddeefb7bb2fa33e6aa28c0b38df091
krishnaKanta2008 <kb0397206@gmail.com>
2025-09-01 22:18:08 +0530

    added vercel config for production

--- app.py
+++ app.py
@@ -675,6 +675,6 @@ def cataract():
         return render_template('cataract.html',username=username)
     return render_template('index.html')
 
-if __name__ == '__main__':
-    create_tables()
-    app.run(debug=True)
+# if __name__ == '__main__':
+#     create_tables()
+#     app.run(debug=True)
